26ae13ffb8fc48690720b7084585822b3d7541f1
kshitijnangarevesit <2023.kshitij.nangare@ves.ac.in>
2024-08-14 01:05:29 +0530

    Add Main.java with HelloWorld class

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        printMessage();
-    }
-    
-    public static void printMessage() {
-        System.out.println("This is a new method.");
     }
 }
