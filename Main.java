16a5307f2b8ec4f6cf45594ada0527d7edc1b81a
kshitijnangarevesit <2023.kshitij.nangare@ves.ac.in>
2024-08-14 01:06:23 +0530

    Add printMessage method to Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        printMessage();
+    }
+    
+    public static void printMessage() {
+        System.out.println("This is a new method.");
     }
 }
